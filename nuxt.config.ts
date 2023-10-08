bd0974cc4ab171fdc44e4b75a5a0f971ab6ab4f9
Foo JH <foojihaw@hotmail.com>
2023-10-08 11:53:09 -0500

    NEW: quasar UI integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8000,
+  },
+  modules: [
+    'nuxt-quasar-ui'
+  ],
+  quasar: { /* */ },
+  
 })
