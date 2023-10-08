89a3e9b66dbed7a5fadb285ceb11092e9d552ce0
Foo JH <foojihaw@hotmail.com>
2023-10-08 12:09:46 -0500

    NEW: quasar UI integration sample framework

--- app.vue
+++ app.vue
@@ -1,5 +1,52 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <q-layout view="lHh LpR fFf">
+
+    <q-header class="bg-primary text-black bg-transparent">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left">
+      <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+          <div class="text-weight-bold">Foo, Ji-Haw</div>
+          <div>foojihaw@</div>
+        </div>
+      </q-img>
+    </q-drawer>
+
+    <q-page-container>
+      <NuxtLayout/>
+    </q-page-container>
+
+  </q-layout>
 </template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="q-ma-md">
+    <NuxtPage />
+  </div>
+</template>
