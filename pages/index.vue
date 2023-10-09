24b7dfc8d8fb48f4a91bf1fc9a711e611f5f3365
Foo JH <foojihaw@hotmail.com>
2023-10-08 21:05:43 -0500

    UPDATED: Introduction literature

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <q-card class="MainCard row" bordered flat>
-      <div class="col-sm-8 col-xs-16">
+  <q-card class="MainCard row" flat>
+      <div class="col-md-8 col-sm-16">
         <q-card-section class="">
+          <div class="text-h5">Hello, World</div>
           <p>
             This is the summation of my perspectives working in technology since graduation in 1998. 
           </p>
@@ -18,23 +19,24 @@
             BEFORE coming to the States, I spent more than 10 years as a software developer in various 
             startups. Researched 3G and IPv6 back in 1999 with Deutsche Telekom. Wrote learning portals 
             serving over 200 schools with Perl. Built on-premise enterprise Cisco-integrated software 
-            used by the likes of BP, Barclays, and Sime Darby using C# and Javascript. The height of
-            my technical experience was the deployment of a massive data discovery tool for the homeland 
+            used by the likes of BP, Barclays, and Sime Darby using C# and Javascript. The culmuination 
+            of my technical experience was the deployment of a massive data discovery tool for the homeland 
             security; details of which cannot be elaborated.
           </p>
           <p>
             DURING my time in the States, my career took a more focused path. I straddled between program
             and product management in various companies within the developer excellence domain, helping 
             developers navigate new technologies and processes brought on by cloud adoption; known also 
-            as Technology Transformation. My time in Amazon launching the Prime Same Day program is 
-            the most insightful both in hard (technology) and soft (critical thinking) skills.
+            as Technology Transformation. At its height I lead a 200-engineer initiative to productize
+            a cloud serivces framework enabling developers more autonomy and transparency over services
+            they care about (databases), while simplifying complexities of elastic compute (kubernetes).  
           </p>
           <p>
             If my skills are what you're looking for, let's talk.
           </p>
         </q-card-section>
       </div>
-      <div class="col-4">
+      <div class="col-4 gt-sm">
         <q-img src="/images/bg-tech-cropped-gray.jpg"/>
       </div>
   </q-card>
